27d684c663aaea86dc97466c97d2b09e04b1212e
saurav2525 <sauravpreet2525@gmail.com>
2018-08-28 15:23:58 +0530

    assignment2

---
+++ assignment 2.py
@@ -0,0 +1,27 @@
+#Print Something
+print("My name is Saurabh")
+#concatinate strings
+str1="Acad"
+str2="view"
+print(str1+str2)
+#print 3 User Defined Variables
+x=input('enter you name')
+y=int(input('enter your roll no'))
+z=float(input('enter your marks'))
+print("name: ",x)
+print("roll no: ",y)
+print("Marks: ",z)
+#Print Let's Get Started
+print("Let's Get Started")
+#Print the following values using placeholders. 
+s="Acadview"
+course="Python"
+fees=5000
+print("s= %s \ncourse= %s \nfees=%d" %(s,course,fees))
+#Find the area of circle 
+pi = 3.14 
+#Take radius as input from user 
+#Print the area of circle
+rad=int(input("enter the radius"))
+Area=pi*rad*rad
+print(Area)
