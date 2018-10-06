720b2864ac161f88ef341c2d606035c8cfdf475c
saurav2525 <41817699+saurav2525@users.noreply.github.com>
2018-10-06 23:39:56 +0530

    Add files via upload

---
+++ Ass-18.py
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+     <meta charset="UTF-8">
+     <title>Cricket Players</title>
+   </head>
+   <body>
+     <h1>Cricket Players</h1>
+     <nav>
+       <ul>
+        <li><a href="#Virat kohli">Thor</a></li>
+        <li><a href="#MS dhoni">Captain america</a></li>
+        <li><a href="#Rohit sharma">Black Panther</a></li>
+       </ul>
+     </nav>
+     <article id="Virat kohli">
+       <h2>Thor</h2>
+       <img src="Virat.jpg" alt="Virat image" width=320>
+       <p>
+         Brother, however I have wronged you, whatever I have done that has led you to do this, I am truly sorry. But these people are innocent, taking their lives will gain you nothing. So take mine, and end this.
+       </p>
+       <ol>
+          <li>Anushka sharma</li>
+
+       </ol>
+     </article>
+     <article id="Ms dhoni">
+        <h2>MS dhoni</h2>
+        <img src="MSdhoni.jpg" width=320>
+        <h3>Characters</h3>
+          <ul>
+              <li>Mahi </li>
+              <li>captain cool</li>
+
+          </ul>
+        <p>
+            “You asked about the Avengers. Y’wanna know the best part about being an Avenger? Having Captain America around you all the time. He just—the guy just brings out the absolute best in people. You want to be good when he’s around. You really do."
+        </p>
+        <h3>Leadership lessons from captain america.</h3>
+        <h1>Be the Hero Your Employees Need (Captain America Style)</h1>
+        <ol>
+           <li>He has a moral center.</li>
+           <li>He stands for something.</li>
+           <li>He shuns the limelight.</li>
+           <li>He adapts to strengths and weaknesses.</li>
+        </ol>
+      </article>
+     <article id="Rs">
+        <h2>Rohit sharma</h2>
+        <img src="Rohitsharma.jpg" alt="rohit sharma" width=320>
+       <p>
+         “You’re telling me that the king of a Third World country runs around in a bulletproof catsuit?”
+       </p>
+       <ol>
+
+       </ol>
+     </article>
+   </body>
+</html>
